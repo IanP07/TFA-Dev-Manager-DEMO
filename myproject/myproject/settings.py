f577de11ddc6c279898b3fbe8d2435017d27bf99
Ian Panait <150859108+IanP07@users.noreply.github.com>
2025-05-04 14:20:44 -0400

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,12 +14,14 @@ from pathlib import Path
 
 import os
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
